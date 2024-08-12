9ff82c2a3c25d328b41d9207b3cc13958fcb5c82
Faith Emmanuel <104941626+FaithEmmamuel@users.noreply.github.com>
2024-08-12 17:24:09 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,50 @@
+var cel = document.getElementById('cel');
+var fah = document.getElementById('fah');
+var kel = document.getElementById('kel');
+
+cel.addEventListener('input', function(){
+    let c = this.value;
+    let f = (c * 9/5)+32;
+    let k = c + 273.15;
+
+    if(!Number.isInteger(f)){
+        f = f.toFixed(4)
+    }
+    fah.value = f;
+    kel.value = k;
+});
+
+fah.addEventListener('input', function(){
+    let f = this.value;
+    let c = (f - 32) * 5/9;
+    let k = c + 273.15;
+
+    if(!Number.isInteger(c)){
+        c = c.toFixed(4)
+    }
+    if(!Number.isInteger(k)){
+        k = k.toFixed(4)
+    }
+    cel.value = c;
+    kel.value = k;
+});
+
+kel.addEventListener('input', function(){
+    let k = this.value;
+    let c = k - 273.15;
+    let f = c * 9/5 + 32;
+
+    if(!Number.isInteger(c)){
+        c = c.toFixed(4)
+    }
+    if(!Number.isInteger(f)){
+        f = f.toFixed(4)
+    }
+    cel.value = c;
+    fah.value = f;
+});
+
+
+let apples = '5';
+let oranges = '7';
+alert(oranges % apples);
